85f72fa93d21122214af3639c881c590e8a15eb8
azaqout <azaqout>
2024-05-05 23:34:09 +0300

    Add project files.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Data;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        ApplicationDbcontext context=new ApplicationDbcontext();
+        public IActionResult Index()
+        {
+            var employee = context.Employees.ToList();
+            return View("index",employee);
+        }
+    }
+}
